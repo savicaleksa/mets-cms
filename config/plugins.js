110aae2f8b4073cc1b37e8dacfc8d87edd8ef419
savicaleksa <aleksa.savic91@gmail.com>
2023-08-04 00:05:37 +0200

    revert

--- config/plugins.js
+++ config/plugins.js
@@ -15,19 +15,5 @@ module.exports = ({ env }) => ({
       },
     },
   },
-  graphql: {
-    enabled: true,
-    config: {
-      endpoint: "/graphql",
-      shadowCRUD: true,
-      playgroundAlways: true,
-      depthLimit: 7,
-      amountLimit: 100,
-      apolloServer: {
-        tracing: false,
-        introspection: true,
-      },
-    },
-  },
   // ...
 });
